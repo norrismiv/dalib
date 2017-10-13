3738c092057a1af11c4d402a2d7a6d01652a602a
Kyle Speck <kojasou@gmail.com>
2017-10-13 11:11:57 -0500

    Add sample projects

---
+++ Samples/MetaDataViewer/MetaDataForm.cs
@@ -0,0 +1,69 @@
+﻿using DALib.Data;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+
+namespace MetaDataViewer
+{
+    public partial class MetaDataForm : Form
+    {
+        public MetaDataForm() => InitializeComponent();
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    OpenMetaDataFile(dialog.FileName);
+                }
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void OpenMetaDataFile(string fileName)
+        {
+            MetaData metaData;
+
+            using (var metaDataStream = File.OpenRead(fileName))
+            {
+                if (metaDataStream.ReadByte() == 0x78 && metaDataStream.ReadByte() == 0x9C)
+                {
+                    using (var decompressedMetaDataStream = new MemoryStream())
+                    using (var decompressionStream = new DeflateStream(metaDataStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedMetaDataStream);
+                        decompressedMetaDataStream.Seek(0, SeekOrigin.Begin);
+                        metaData = new MetaData(decompressedMetaDataStream);
+                    }
+                }
+                else
+                {
+                    metaDataStream.Seek(-2, SeekOrigin.Current);
+                    metaData = new MetaData(metaDataStream);
+                }
+            }
+
+            var entryNodes = new TreeNode[metaData.Entries.Count];
+            for (var i = 0; i < metaData.Entries.Count; ++i)
+            {
+                var entry = metaData[i];
+                var valueNodes = new TreeNode[entry.Values.Count];
+                for (var j = 0; j < entry.Values.Count; ++j)
+                {
+                    valueNodes[j] = new TreeNode(entry[j]);
+                }
+                entryNodes[i] = new TreeNode(entry.Key, valueNodes);
+            }
+
+            Text = $"{Path.GetFileName(fileName)} - Meta Data Viewer";
+            entriesList.Nodes.Clear();
+            entriesList.Nodes.AddRange(entryNodes);
+        }
+    }
+}
---
+++ Samples/UnpackDataFile/Program.cs
@@ -0,0 +1,56 @@
+﻿using DALib.Data;
+using System;
+using System.IO;
+
+namespace UnpackDataFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: UnpackDataFile.exe <dataFilePath> <destinationPath>");
+                return;
+            }
+
+            var dataFilePath = args[0];
+
+            if (!File.Exists(dataFilePath))
+            {
+                Console.WriteLine("Data file does not exist.");
+                return;
+            }
+
+            DataFile dataFile;
+
+            try
+            {
+                dataFile = DataFile.Open(dataFilePath);
+            }
+            catch
+            {
+                Console.WriteLine("Could not open data file.");
+                return;
+            }
+
+            var destinationPath = args[1];
+            Directory.CreateDirectory(destinationPath);
+
+            Console.WriteLine($"Unpacking {dataFile.Entries.Count} files...");
+
+            foreach (var entry in dataFile)
+            {
+                var destinationFileName = Path.Combine(destinationPath, entry.EntryName);
+                using (var entryStream = entry.Open())
+                using (var fileStream = File.OpenWrite(destinationFileName))
+                {
+                    entryStream.CopyTo(fileStream);
+                }
+                Console.WriteLine($"    Unpacked {entry.EntryName}");
+            }
+
+            Console.WriteLine($"Finished unpacking {dataFile.Entries.Count} files.");
+        }
+    }
+}
