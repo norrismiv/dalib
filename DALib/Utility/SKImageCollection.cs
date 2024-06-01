258929ab42b000a1f83f2e54226aef9449662fe9
sichii <despereaux@live.com>
2024-06-01 16:28:15 -0400

    remove null frames

--- DALib/Utility/SKImageCollection.cs
+++ DALib/Utility/SKImageCollection.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using SkiaSharp;
+// ReSharper disable ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
 
 namespace DALib.Utility;
 
 /// <summary>
 ///     Represents a disposable collection of SKImages.
 /// </summary>
-public class SKImageCollection(IEnumerable<SKImage> images) : Collection<SKImage>(images.ToList()), IDisposable
+public class SKImageCollection(IEnumerable<SKImage> images) : Collection<SKImage>(
+                                                                  images.Where(frame => frame is not null)
+                                                                        .ToList()),
+                                                              IDisposable
 {
     /// <inheritdoc />
     public virtual void Dispose()
